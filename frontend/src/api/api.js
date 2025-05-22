032c674141117ac02861e2c274f68f52ccb3ab9d
shagufta28 <saifishagufta82@gmail.com>
2025-05-22 21:41:51 +0530

    deployed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ai-story-generator-rtmh.vercel.app/api',
 });
 
 API.interceptors.request.use((req) => {
